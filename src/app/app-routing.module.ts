8b63c7432604e2e4064666e04e6db3c912151252
mario <vigossalamanca@hotmail.com>
2022-03-01 00:20:45 -0600

    final del rutas hijas (no lazyload) v36

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,23 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
-import { NopagefoundComponent } from './nopagefound/nopagefound.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
 
-import { PagesComponent } from './pages/pages.component';
-import { ProgressComponent } from './pages/progress/progress.component';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+import { PagesRoutingModule } from './pages/app-pages.routing.module';
+
 
 const routes: Routes = [
-  {path:'',
-  component:PagesComponent,
-  children: [
-    {path: 'dashboard', component: DashboardComponent},
-    {path: 'progress', component: ProgressComponent},
-    {path: 'grafica1', component: Grafica1Component},
-    {path: '', redirectTo: '/dashboard', pathMatch:'full'},
-  ]
-  },
+
 
   {path: 'register', component: RegisterComponent},
   {path: 'login', component: LoginComponent},
@@ -28,7 +18,10 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+    PagesRoutingModule
+    ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
